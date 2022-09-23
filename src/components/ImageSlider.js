c03a92b2493e955ac57379862263ab2f0b5bc327
ahmetfdurmaz <cheror07@gmail.com>
2022-09-23 15:27:05 +0200

    making autoslider and dots

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { SliderData } from "./SliderData";
 import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from "react-icons/fa";
 
@@ -15,19 +15,50 @@ const ImageSlider = ({ slides }) => {
     setCurrent(current === 0 ? length - 1 : current - 1);
   };
 
+  // Auto Slide
+  let slideInterval;
+  function auto() {
+    slideInterval = setInterval(next, 3500);
+  }
+
+  useEffect(() => {
+    const autoScroll = true;
+    if (autoScroll) {
+      auto();
+    }
+    return () => clearInterval(slideInterval);
+  }, [current]);
+
   return (
     <div className="slider">
-      <div className="p">
-        <h4>{current + 1}</h4>
-      </div>
       <FaArrowAltCircleLeft className="left-arrow" onClick={prev} />
       <FaArrowAltCircleRight className="right-arrow" onClick={next} />
       <div className="buttons">
-        <button type="radio" className="button" onClick={() => setCurrent(0)} />
-        <button type="radio" className="button" onClick={() => setCurrent(1)} />
-        <button type="radio" className="button" onClick={() => setCurrent(2)} />
-        <button type="radio" className="button" onClick={() => setCurrent(3)} />
-        <button type="radio" className="button" onClick={() => setCurrent(4)} />
+        <button
+          type="radio"
+          className={current === 0 ? "button active" : "button"}
+          onClick={() => setCurrent(0)}
+        />
+        <button
+          type="radio"
+          className={current === 1 ? "button active" : "button"}
+          onClick={() => setCurrent(1)}
+        />
+        <button
+          type="radio"
+          className={current === 2 ? "button active" : "button"}
+          onClick={() => setCurrent(2)}
+        />
+        <button
+          type="radio"
+          className={current === 3 ? "button active" : "button"}
+          onClick={() => setCurrent(3)}
+        />
+        <button
+          type="radio"
+          className={current === 4 ? "button active" : "button"}
+          onClick={() => setCurrent(4)}
+        />
       </div>
 
       {SliderData.map((slide, index) => {
